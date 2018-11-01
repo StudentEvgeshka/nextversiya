700b79cc32d2c19deeb3323c6b1c9da85a94a14f
StudentEvgeshka <43698839+StudentEvgeshka@users.noreply.github.com>
2018-11-02 01:18:30 +0200

    123

---
+++ js/script.js
@@ -0,0 +1,10 @@
+.uvelich{
+-moz-transition: all 0.3s ease-out;
+ -o-transition: all 0.3s ease-out;
+ -webkit-transition: all 0.3s ease-out;
+}
+.uvelich:hover{
+ -webkit-transform: scale(1.1);
+ -moz-transform: scale(1.1);
+ -o-transform: scale(1.1);
+ }
